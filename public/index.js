7b8a4141f165d212c3dc42d71d894423e9244c2c
Mila1444 <66134690+Mila1444@users.noreply.github.com>
2020-07-05 21:22:55 -0400

    added styling

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-let myMap = L.map('map').setView([44.48, -73.21], 15);
+let myMap = L.map('map').setView([44.48, -73.21], 14.5);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -25,9 +25,8 @@ fetch('https://yelpingtonapi.herokuapp.com/api/restaurants').catch((err) => {
             restList.appendChild(item)
             console.log(restCollect)
 
-            let coords = JSON.parse(rest.coords)
-
-            let latLngArr = [coords[0], coords[1]]
+            
+            let latLngArr = [rest.coords[0], rest.coords[1]]
             marker = L.marker(latLngArr).addTo(myMap).bindPopup(`<a href="/restaurant#${rest.id}">${rest.name}</a>`)
 
             marker.addEventListener('mouseover', () => {
--- public/restaurant.js
+++ public/restaurant.js
@@ -1,6 +1,12 @@
 
 let restInfo = document.getElementById("restaurant-info")
 let restId = document.location.hash.slice(1)
+let restName = document.getElementById("rest-name")
+let restAddress = document.getElementById("rest-address")
+let restPhone = document.getElementById("rest-phone")
+let restHours = document.getElementById("rest-hours")
+let restWebsite = document.getElementById("rest-website")
+let restNotes = document.getElementById("rest-notes")
 //console.log(document.location.hash.slice(1))
 //let restId = document.location.search.slice(1) - if I wanted to use search query instead of a hash.
 //console.log(document.location.search)
@@ -19,29 +25,29 @@ fetch(`https://yelpingtonapi.herokuapp.com/api/restaurants/${restId}`).catch((er
     .then((rest) => {
 
         console.log(rest)
-        let name = document.createElement('h1')
+        let name = document.createElement('h2')
         name.innerHTML = `${rest.name}`
-        let address = document.createElement('h4')
+        let address = document.createElement('h5')
         address.innerHTML = `${rest.address}`
-        let phone = document.createElement('h4')
+        let phone = document.createElement('h5')
         phone.innerHTML = `${rest.phone}`
-        let hours = document.createElement('h4')
+        let hours = document.createElement('ul')
         hours.innerHTML = `${rest.hours}`
-        let website = document.createElement('h4')
+        let website = document.createElement('h5')
         website.innerHTML = `${rest.website}`
-        let notes = document.createElement('h4') // can create list ul
+        let notes = document.createElement('ul') // can create list ul
         notes.innerHTML = `${rest.notes}`
 
         restInfo.appendChild(name)
         restInfo.appendChild(address)
         restInfo.appendChild(phone)
-        restInfo.appendChild(hours)
         restInfo.appendChild(website)
+        restInfo.appendChild(hours)
         restInfo.appendChild(notes)
 
-        let coords = JSON.parse(rest.coords)
+        
 
-        let myMap = L.map('map-rest').setView([coords[0], coords[1]], 15);
+        let myMap = L.map('map-rest').setView([rest.coords[0], rest.coords[1]], 18);
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
@@ -50,7 +56,7 @@ fetch(`https://yelpingtonapi.herokuapp.com/api/restaurants/${restId}`).catch((er
 
         let marker = L.marker([lat, lon])
 
-        let latLngArr = [coords[0], coords[1]]
+        let latLngArr = [rest.coords[0], rest.coords[1]]
         marker = L.marker(latLngArr).addTo(myMap).bindPopup(`<a href="/restaurant#${rest.id}">${rest.name}</a>`)
 
 
