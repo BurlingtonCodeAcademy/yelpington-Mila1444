90cbc559189021782600c13371b4c488447f301c
Mila1444 <66134690+Mila1444@users.noreply.github.com>
2020-07-30 00:21:09 -0400

    improved styling, added a working comments section and build api

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -5,8 +5,9 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap)
 
-//variables
+//declares variable for DOM restaurant element
 let restList = document.getElementById("restaurant-list")
+//declares lat/long variables
 let lat = 0
 let lon = 0
 
@@ -20,25 +21,28 @@ fetch('https://yelpingtonapi.herokuapp.com/api/restaurants').catch((err) => {
     .then((restCollect) => {
         //creates restaurant list
         restCollect.forEach(rest => {
+            //creates list element
             let item = document.createElement('li')
+            //creates html text for restaurant list
             item.innerHTML = `<a href="/restaurant#${rest.id}">${rest.name}</a>`
+            //appends html element to restaurant list
             restList.appendChild(item)
             //adds markers
-            
             let latLngArr = [rest.coords[0], rest.coords[1]]
+            //adds popup feature
             marker = L.marker(latLngArr).addTo(myMap).bindPopup(`<a href="/restaurant#${rest.id}">${rest.name}</a>`).on('click', function () {
                 event.preventDefault();
                 window.location = `/restaurant#${rest.id}`
                 console.log(`/restaurant#${rest.id}`)
             })
-            
+            //adds scroll over feature
             marker.on('mouseover', function () {
                 this.openPopup()
             })
         });
     })
 
-//marker variable
+//declares marker variable
 let marker = L.marker([lat, lon])
 
 
--- public/javascript/restaurant.js
+++ public/javascript/restaurant.js
@@ -1,7 +1,7 @@
 //getting location to fetch data 
 let restId = document.location.hash.slice(1)
 
-//variable for html elements
+//variable for html/DOM elements
 let restInfo = document.getElementById("restaurant-info")
 let restName = document.getElementById("rest-name")
 let restAddress = document.getElementById("rest-address")
@@ -11,9 +11,9 @@ let restWebsite = document.getElementById("rest-website")
 let restNotes = document.getElementById("rest-notes")
 let commentsForm = document.getElementById("comments-form")
 let commentsContainer = document.getElementById("comments-container")
-let allComments = document.getElementById("all-comments")
 
-//variables
+
+//declares lat/long variables
 let lat = 0
 let lon = 0
 
@@ -65,10 +65,55 @@ fetch(`https://yelpingtonapi.herokuapp.com/api/restaurants/${restId}`).catch((er
         })
     })
 
+//declares array variable that will contain comments
+let commentArray
+
+//checks if local storage is empty/null and creates an empty array when there are no comments to display
+if (localStorage.getItem(restId) === null) {
+    commentArray = []
+} else {
+    commentArray = JSON.parse(window.localStorage.getItem(restId))
+}
+
+//checks if comment array is empty and display messages acordingly
+if (commentArray.length > 0) {
+    //iterates over comment array and displays html text
+    let commentsHtml = commentArray.map((comment) => {
+        return `<p>Name: ${comment.userName}</p><p>Email: ${comment.emailValue}</p><p>Comments: ${comment.commentValue}</p><br />`
+    })
+    //declares empty string variable
+    let htmlString = "";
+    //iterates over html text and removes comma
+    commentsHtml.forEach((comment) => htmlString += comment)
+    //displays new html text
+    commentsContainer.innerHTML = htmlString
+    //text for no comments
+} else {
+    commentsContainer.innerHTML = '<p>No comments yet. Add the first comment!</p>'
+}
+
 //function to submit form    
 function submitComment(event) {
+    //prevents default event
     event.preventDefault();
-    allComments.textContent = "Your Comment"
+    //creates variables for form values
+    let user = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let comment = document.getElementById('comment').value;
+    //creates an object with all three form values
+    let commentObject = {
+        userName: user,
+        emailValue: email,
+        commentValue: comment
+    }
+    //pushes new comment object into comment array
+    commentArray.push(commentObject)
+    //creates string to be used in local storage
+    let commentString = JSON.stringify(commentArray)
+    // sets local storage using restaurant id and string previously created
+    window.localStorage.setItem(restId, commentString)
+    //reloads page to display last comment added
+    location.reload()
 }
 
 //adds event listener to form
